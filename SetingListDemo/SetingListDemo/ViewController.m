f91da64d96f89fa7ea0420a52d54e76e58690473
周鑫 <15901263220@163.com>
2019-05-06 16:07:43 +0800

    项目初始化

---
+++ SetingListDemo/SetingListDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  SetingListDemo
+//
+//  Created by 周鑫 on 2019/5/6.
+//  Copyright © 2019 ZX. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
---
+++ SetingListDemo/SetingListDemoTests/SetingListDemoTests.m
@@ -0,0 +1,37 @@
+//
+//  SetingListDemoTests.m
+//  SetingListDemoTests
+//
+//  Created by 周鑫 on 2019/5/6.
+//  Copyright © 2019 ZX. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SetingListDemoTests : XCTestCase
+
+@end
+
+@implementation SetingListDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
